41d405624a8e00d6024602bb363ddd7453db3696
Artem Ershov <spider_artem@mail.ru>
2023-01-14 20:44:55 +0300

    Complete App

---
+++ RazorPizzeria/Migrations/20230114172303_BaseToFinalPrice.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPizzeria.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseToFinalPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BasePrice",
+                table: "PizzaOrders",
+                newName: "FinalPrice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FinalPrice",
+                table: "PizzaOrders",
+                newName: "BasePrice");
+        }
+    }
+}
--- RazorPizzeria/Models/PizzaOrder.cs
+++ RazorPizzeria/Models/PizzaOrder.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string PizzaName { get; set; }
-        public float BasePrice{ get; set; }
+        public float FinalPrice{ get; set; }
     }
 }
